256fe60d56a52362f4640d502f9f23a088e9b7cb
Athesto <gamez.live@gmail.com>
2021-08-20 16:51:27 -0500

    order re and im

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -12,5 +12,5 @@ void division(complex c1, complex c2, complex *c3)
 	double divisor = pow(c2.re, 2) + pow(c2.im, 2);
 
 	c3->re = ((c1.re * c2.re) + (c1.im * c2.im)) / divisor;
-	c3->im = ((c1.re * c2.im) - (c1.im * c2.re)) / divisor;
+	c3->im = ((c1.im * c2.re) - (c1.re * c2.im)) / divisor;
 }
