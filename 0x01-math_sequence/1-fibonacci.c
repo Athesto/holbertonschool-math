7529de116aeae6e9fdde08ce942e203415f6d777
Athesto <gamez.live@gmail.com>
2021-08-21 00:11:08 -0500

    no sqrt, fewer variables

--- 0x01-math_sequence/1-fibonacci.c
+++ 0x01-math_sequence/1-fibonacci.c
@@ -1,7 +1,6 @@
 #include "fibonacci.h"
 #include <math.h>
 #include <stdio.h>
-#define ABS(x) ((x) >= 0 ? (x) : -(x))
 
 
 t_cell *add_list(t_cell **head, int elt);
@@ -12,16 +11,14 @@ t_cell *add_list(t_cell **head, int elt);
 t_cell *Fibonnaci(void)
 {
 	t_cell *head = NULL;
-	/* const double GOLD_NUMBER = (0.5 + sqrt(5) / 2); */
 
 	add_list(&head, 1);
 	add_list(&head, 1);
-	add_list(&head, head->elt + head->next->elt);
 
 	while (1)
 	{
 		add_list(&head, head->elt + head->next->elt);
-		if (ABS(gold_number(head) - gold_number(head->next)) < 1 / 1e10)
+		if (gold_number(head) == gold_number(head->next))
 			break;
 	}
 
